3a4279d55750ab1f11e5bc5b646178aed2771f7a
balbir <prasadbalbir1056@gmail.com>
2026-09-19 10:47:11 +0530

    stop sync from deleting results and logs on the box

--- sync.sh
+++ sync.sh
@@ -7,14 +7,15 @@ DEST="${1:-balbir.prasad@10.0.62.168:work/mil}"   # ramanujan, lab network
 SSH_PORT="${SSH_PORT:-2022}"
 cd "$(dirname "$0")"
 rsync -av --delete -e "ssh -p $SSH_PORT" \
-  --exclude data --exclude results --exclude .git --exclude '__pycache__' --exclude '*.ipynb_checkpoints' \
+  --exclude data --exclude 'results*' --exclude '*.log' --exclude .git --exclude '__pycache__' \
+  --exclude '*.ipynb_checkpoints' \
   ./ "$DEST/"
 cat <<MSG
 
 Synced to $DEST. On the server:
 
   cd ~/work/mil
-  CUDA_VISIBLE_DEVICES=3 nohup ~/wrf/wrfEnv/bin/python train.py \
-      --features data/pda-ft --out results --quiet > train.log 2>&1 &
+  CUDA_VISIBLE_DEVICES=3 nohup ~/wrf/wrfEnv/bin/python -u train.py \
+      --features data/pda-ft --out results --quiet --threads 4 > train.log 2>&1 &
   tail -f train.log
 MSG
